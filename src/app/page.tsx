0987e31557b6f15763a61e92bf59071c8cfc13ba
AlSherif Khalaf <73169053+alsherif-khalaf@users.noreply.github.com>
2023-04-10 10:19:27 +0200

    sumbit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import {
   getAllCats,
-  getModels,
+  getOptionsChild,
   getSubCategoryProperties,
 } from "../../utils/data";
 import SearchableDropdownMenu from "./components/SearchableDropdownMenu";
@@ -12,6 +12,8 @@ export default async function Home({
 }: {
   searchParams?: { [key: string]: string | string[] | undefined };
 }) {
+
+
   // Get The categories directly as this page is a server component
   const categories = await getAllCats();
 
@@ -30,14 +32,13 @@ export default async function Home({
     : null;
 
   // Get models by brand from searchParams  '/?brand=55'
-
   const brandID = searchParams?.brand as string | undefined;
-  const models = brandID ? await getModels(brandID) : null;
+  const models = brandID ? await getOptionsChild(brandID) : null;
 
-  if (models) {
-    console.log("user selected brand", models.data[0].options);
-    // console.log(subCategoryProperties)
-  }
+  // Get types by selected model from searchParams  '/?brand=55'
+  const modelID = searchParams?.model as string | undefined;
+  const types = modelID ? await getOptionsChild(modelID) : null;
+
 
   return (
     <div className="p-4 all_items w-full min-h-[200px] bg-white rounded-xl">
@@ -49,6 +50,7 @@ export default async function Home({
             placeholder="التصنيفات الرئيسية"
             type="category"
           />
+
           {/* Sub Categories */}
           <SearchableDropdownMenu
             options={subCategory?.children}
@@ -59,27 +61,41 @@ export default async function Home({
           {/* Properties */}
 
           {subCategoryProperties?.data.map((cat: any) => (
-            <div key={cat.slug}>
+            <div key={cat?.slug}>
               <SearchableDropdownMenu
                 options={cat?.options}
                 placeholder={cat?.name}
                 type={cat?.slug}
                 multi={cat.slug !== "brand"}
               />
-              {cat.slug === "brand" && (
+
+              {cat.slug === "brand" && models && (
                 <SearchableDropdownMenu
-                  options={models?.data[0].options}
-                  placeholder="الموديلات"
+                  options={models?.data[0]?.options}
+                  placeholder="الموديل"
                   type="model"
-                  multi
                 />
               )}
+
+              {cat.slug === "brand" && types !== null && (
+                <SearchableDropdownMenu
+                  options={types?.data[0]?.options}
+                  placeholder="النوع"
+                  type={types?.data[0]?.slug}
+                />
+              )}
+
             </div>
           ))}
         </div>
+
+        {/* Sumbit buttom or form should take the url searchParams and confirm a query api request */}
+        
         <button className="w-full my-4 bg-black text-white  font-light py-4 px-6 ">
           بحـث
         </button>
+
+
       </ClientOnly>
     </div>
   );
